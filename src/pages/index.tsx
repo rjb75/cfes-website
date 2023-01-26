891473b5822a1014b9fe0191dd80ed266f0b4a31
Robert Brown <brown.rjb75@gmail.com>
2023-01-25 22:18:06 -0700

    feat: initial cfes content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,145 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import "../styles/common.scss";
+import "./index.scss";
+import CFESBanner from "../images/cfes_logo.png";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
+    <div className="outer-container">
+      <div className="website-container">
+        <div className="website-header">
+          <img src={CFESBanner} alt="CFES Logo" className="logo" />
+        </div>
+        <div className="website-message">
+          <p>
+            Hi friends! We are so sorry for the inconvenience, but our website
+            is down right now.
+          </p>
+          <ul className="quick-resources">
+            <li>
+              If you are looking for the CSE 2023 website,{" "}
+              <a href="https://cse2023.com" target={"_blank"}>
+                click here
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+              .
+            </li>
+            <li>
+              If you are looking for the CEC 2023 website,{" "}
+              <a href="https://cec2023.ca" target={"_blank"}>
+                click here
+              </a>
+              .
+            </li>
+            <li>
+              If you are looking for any specific resources, check out the{" "}
+              <a
+                href="https://drive.google.com/drive/folders/1w-m3voVWbAM8TgigXoxgbxCiHKn36O6m"
+                target={"_blank"}
+              >
+                external drive
+              </a>
+              .
+            </li>
+          </ul>
+          <p>
+            If you have any questions, please reach out to{" "}
+            <a href="mailto:vpc@cfes.ca">vpc@cfes.ca</a> and we will find you
+            what you need!
+          </p>
+        </div>
+        <div className="website-resources">
+          <div className="calls-to-action">
+            <p>
+              From coast to coast to coast, we acknowledge the ancestral and
+              unceded territories of all the Inuit, Métis and First Nations
+              people that call this land home. We acknowledge Indigenous people
+              and knowledge keepers have been doing engineering for thousands of
+              years but have not historically been recognized for their
+              expertise. We affirm our commitment and responsibility as settlers
+              to amplifying Indigenous ways of knowing and access to engineering
+              in our work.
+            </p>
+            <a href="https://ehprnh2mwo3.exactdn.com/wp-content/uploads/2021/01/Calls_to_Action_English2.pdf">
+              Truth & Reconciliation Commission of Canada: Calls to Action
+            </a>
+            <p>
+              D’un océan à l’autre, nous reconnaissons les territoires
+              ancestraux et non cédés de tous les Inuits, Métis et Premières
+              Nations qui habitent cette terre. Nous reconnaissons que les
+              peuples indigènes et les gardiens des connaissances faisaient de
+              l’ingénierie depuis des milliers d’années, mais n’ont pas toujours
+              été reconnut pour leur expertise. Nous affirmons notre engagement
+              et notre responsabilité en tant qu’organisation d’ingénierie
+              coloniale à amplifier les façons indigènes de connaître et l’accès
+              à l’ingénierie dans notre travail. Nous sommes solidaires
+              aujourd’hui et tous les jours avec tous les guerriers indigènes.
+              Nous sommes tous visés par les traités.
+            </p>
+            <a href="https://ehprnh2mwo3.exactdn.com/wp-content/uploads/2021/04/4-Appels_a_l-Action_French.pdf">
+              Commission de vérité et réconciliation du Canada : Appels à
+              l’action{" "}
+            </a>
+          </div>
+          <div className="resource-links">
+            <h3 className="resource-header">Resources</h3>
+            <ul className="resource-list">
+              <li>
+                <a
+                  href="https://drive.google.com/drive/folders/1vFBdwC-7V79QQYbYXdmAGENF4Ddg3rPk?usp=sharing"
+                  target={"_blank"}
+                >
+                  Policy Manual
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/12qE49B-nHSgL1hk7I1GYyp8WvluXukOB/view?usp=sharing"
+                  target={"_blank"}
+                >
+                  Constitution
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/open?id=1w-m3voVWbAM8TgigXoxgbxCiHKn36O6m"
+                  target={"_blank"}
+                >
+                  Full Drive
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/1vxUrmMIJQVQRBFwaZbuedXlvLtVGxHOh/view?usp=sharing"
+                  target={"_blank"}
+                >
+                  CFES Acronym Cheat Sheet
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/1ieb8ixH-okIoforHQOEYbnQIWGOhywQ6/view?usp=sharing"
+                  target={"_blank"}
+                >
+                  Bylaws
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/drive/folders/1_46MIYY4MkcZtOAZ60zWZGp0Ecylu_TV?usp=sharing"
+                  target={"_blank"}
+                >
+                  Reports
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
