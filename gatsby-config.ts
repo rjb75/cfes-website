fd6d15a400b176e8024b9e5ef9f48a515881e520
Robert Brown <brown.rjb75@gmail.com>
2023-01-28 14:24:42 -0700

    feat: add localization

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,49 @@
 import type { GatsbyConfig } from "gatsby";
+require("dotenv").config();
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `CFES Website`,
-    siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `locale`,
+        path: `${__dirname}/src/locales/`,
+      },
     },
-    __key: "pages"
-  }]
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`,
+        languages: [`en`, `fr`],
+        defaultLanguage: `en`,
+        useLangKeyLayout: false,
+        siteUrl: process.env.SITE_URL,
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,84 +1,71 @@
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import { graphql, HeadFC, PageProps } from "gatsby";
 import "../styles/common.scss";
 import "./index.scss";
 import CFESBanner from "../images/cfes_logo.png";
+import { Trans, useTranslation } from "react-i18next";
+import { Link, useI18next } from "gatsby-plugin-react-i18next";
+import { languages as langConsts } from "../utils/languageConstants";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const { t } = useTranslation();
+  const { language, languages, originalPath } = useI18next();
+
   return (
     <div className="outer-container">
       <div className="website-container">
         <div className="website-header">
           <img src={CFESBanner} alt="CFES Logo" className="logo" />
+          {languages.map((lang) => {
+            if (lang !== language) {
+              return (
+                <Link to={originalPath} language={lang} key={lang} className="lang-link">
+                  {langConsts[lang]}
+                </Link>
+              );
+            }
+          })}
         </div>
         <div className="website-message">
-          <p>
-            Hi friends! We are so sorry for the inconvenience, but our website
-            is down right now.
-          </p>
+          <p>{t("outage-message")}</p>
           <ul className="quick-resources">
             <li>
-              If you are looking for the CSE 2023 website,{" "}
-              <a href="https://cse2023.com" target={"_blank"}>
-                click here
-              </a>
-              .
+              <Trans i18nKey={"cse-link"} ns={"translation"}>
+                <a href="https://cse2023.com" target={"_blank"}>
+                  click here
+                </a>
+              </Trans>
             </li>
             <li>
-              If you are looking for the CEC 2023 website,{" "}
-              <a href="https://cec2023.ca" target={"_blank"}>
-                click here
-              </a>
-              .
+              <Trans i18nKey={"cec-link"} ns={"translation"}>
+                <a href="https://cec2023.ca" target={"_blank"}>
+                  click here
+                </a>
+              </Trans>
             </li>
             <li>
-              If you are looking for any specific resources, check out the{" "}
-              <a
-                href="https://drive.google.com/drive/folders/1w-m3voVWbAM8TgigXoxgbxCiHKn36O6m"
-                target={"_blank"}
-              >
-                external drive
-              </a>
-              .
+              <Trans i18nKey={"external-drive"} ns={"translation"}>
+                <a
+                  href="https://drive.google.com/drive/folders/1w-m3voVWbAM8TgigXoxgbxCiHKn36O6m"
+                  target={"_blank"}
+                >
+                  click here
+                </a>
+              </Trans>
             </li>
           </ul>
           <p>
-            If you have any questions, please reach out to{" "}
-            <a href="mailto:vpc@cfes.ca">vpc@cfes.ca</a> and we will find you
-            what you need!
+            <Trans i18nKey={"contact-link"}>
+              {" "}
+              <a href="mailto:vpc@cfes.ca">vpc@cfes.ca</a>
+            </Trans>
           </p>
         </div>
         <div className="website-resources">
           <div className="calls-to-action">
-            <p>
-              From coast to coast to coast, we acknowledge the ancestral and
-              unceded territories of all the Inuit, Métis and First Nations
-              people that call this land home. We acknowledge Indigenous people
-              and knowledge keepers have been doing engineering for thousands of
-              years but have not historically been recognized for their
-              expertise. We affirm our commitment and responsibility as settlers
-              to amplifying Indigenous ways of knowing and access to engineering
-              in our work.
-            </p>
-            <a href="https://ehprnh2mwo3.exactdn.com/wp-content/uploads/2021/01/Calls_to_Action_English2.pdf">
-              Truth & Reconciliation Commission of Canada: Calls to Action
-            </a>
-            <p>
-              D’un océan à l’autre, nous reconnaissons les territoires
-              ancestraux et non cédés de tous les Inuits, Métis et Premières
-              Nations qui habitent cette terre. Nous reconnaissons que les
-              peuples indigènes et les gardiens des connaissances faisaient de
-              l’ingénierie depuis des milliers d’années, mais n’ont pas toujours
-              été reconnut pour leur expertise. Nous affirmons notre engagement
-              et notre responsabilité en tant qu’organisation d’ingénierie
-              coloniale à amplifier les façons indigènes de connaître et l’accès
-              à l’ingénierie dans notre travail. Nous sommes solidaires
-              aujourd’hui et tous les jours avec tous les guerriers indigènes.
-              Nous sommes tous visés par les traités.
-            </p>
-            <a href="https://ehprnh2mwo3.exactdn.com/wp-content/uploads/2021/04/4-Appels_a_l-Action_French.pdf">
-              Commission de vérité et réconciliation du Canada : Appels à
-              l’action{" "}
+            <p>{t("trc-message")}</p>
+            <a href={language == 'en' ? "https://ehprnh2mwo3.exactdn.com/wp-content/uploads/2021/01/Calls_to_Action_English2.pdf" : "https://ehprnh2mwo3.exactdn.com/wp-content/uploads/2021/04/4-Appels_a_l-Action_French.pdf"}>
+              {t("trc-link")}
             </a>
           </div>
           <div className="resource-links">
@@ -142,4 +129,20 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>CFES | FCEG | Canadian Federation of Engineering Students</title>;
+export const Head: HeadFC = () => (
+  <title>CFES | FCEG | Canadian Federation of Engineering Students</title>
+);
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
